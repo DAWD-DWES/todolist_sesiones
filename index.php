5f37d88884676f8b8d8b67cc8295266be8508fcc
jivanrod <jivanrod@gmail.com>
2025-01-12 17:28:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ if (filter_has_var(INPUT_POST, 'crear_tarea')) { // Si se solicita la creación 
 } else if (filter_has_var(INPUT_POST, 'completar_tarea')) { // Si se solicita que se complete una tarea
     $tareaId = filter_input(INPUT_POST, 'tarea_id', FILTER_VALIDATE_INT); // Se lee el número de tarea (uno más que el índice real)          
     $tareas[$tareaId - 1]['estado'] = true; // Se cambia el estado de completado de la tarea
+} else if (filter_input(INPUT_GET, 'limpiar_tareas')) {
+    $tareas = [];
 }
 
 $_SESSION['tareas'] = $tareas;
@@ -38,7 +40,8 @@ $_SESSION['tareas'] = $tareas;
                     <legend>Nueva Tarea:</legend>
                     <div class="form-section">
                         <label for="tarea">Tarea:</label>
-                        <input id="tarea" type="text" name="tarea" value="<?= ($tareaErr ?? false) ? $tarea : '' ?>">
+                        <input id="tarea" type="text" name="tarea" placeholder="Escribe tu tarea (5-50 caracteres)" 
+                               value="<?= ($tareaErr ?? false) ? $tarea : '' ?>">
                         <span class="error <?= ($tareaErr ?? false) ? 'error-visible' : '' ?>">
                             <?= constant("TAREA_INVALIDA") ?>
                         </span>
@@ -76,10 +79,10 @@ $_SESSION['tareas'] = $tareas;
                             <input id="tarea" type="number" min="1" max=<?= count($tareas) ?> value="1" name="tarea_id">
                             <input class="submit blue" type="submit" value="Tarea Completada" name='completar_tarea'/>
                             <input class="submit blue" type="submit" value="Tarea Borrada" name='borrar_tarea'/>
-                            <input class="submit red" type="submit" formmethod = "GET" formaction="<?= "{$_SERVER['PHP_SELF']}?limpiar_tareas" ?>"  value="Vaciar Agenda">
+                            <input class="submit red" type="submit" formaction="<?= "{$_SERVER['PHP_SELF']}?limpiar_tareas" ?>"  value="Vaciar Agenda">
                             <!-- Otra manera de enviar la petición al servidor con un mensaje GET-->   
                             <!-- Esta forma no requiere que el botón sea de tipo submit -->
-                            <!-- <a href="<?= "{$_SERVER['PHP_SELF']}?limpiar_tareas" ?>"><input class="submit red" value="Vaciar Agenda"></a> -->                     
+                            <!-- <a href="<?= "{$_SERVER['PHP_SELF']}?limpiar_tareas" ?>"><input type="button" class="submit red" value="Vaciar Agenda"></a> -->                     
                         </div> 
                     </fieldset>
                 <?php else: ?>
